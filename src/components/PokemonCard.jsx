b494e0001fc336fd3cc68057d2117d31a7df80a4
memed <memedjjebri@gmail.com>
2024-03-26 21:58:40 +0100

    added component.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -8,3 +8,42 @@ function PokemonCard() {
 }
 
 export default PokemonCard;
+
+
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const PokemonCard = () => {
+  
+  const [pokemon, setPokemon] = useState(pokemonList[0]);
+
+  
+  const changePokemon = () => {
+    setPokemon(pokemon === pokemonList[0] ? pokemonList[1] : pokemonList[0]);
+  };
+
+  return (
+    <div>
+
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      
+      <figcaption>{pokemon.name}</figcaption>
+      
+      <button onClick={changePokemon}>Changer de Pokémon</button>
+    </div>
+  );
+};
+
+export default PokemonCard;
