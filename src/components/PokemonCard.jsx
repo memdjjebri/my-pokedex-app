95c1c375fd1391a0d79175451bcb48451b0078f1
memed <memedjjebri@gmail.com>
2024-03-26 17:37:34 +0100

    added PokemonCard component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,10 @@
+function PokemonCard() {
+    return (
+        <>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+            <figcaption>Bulbasaur</figcaption>
+        </>
+    );
+}
+
+export default PokemonCard;
