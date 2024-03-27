ee83c44cef6c2f8284a0ae46abb1686090c51a94
memed <memedjjebri@gmail.com>
2024-03-27 23:26:12 +0100

    add to component.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import PokemonCard from './components/PokemonCard';
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+import PokemonCard from "./components/PokemonCard"
 
 function App() {
-
-  const pokemon = { name: "mew" };
-
   return (
-    <div className="App">
-      <PokemonCard pokemon={pokemon} />
-    </div>
+    <>
+      <PokemonCard/>
+    </>
   );
 }
 
